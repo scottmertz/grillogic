90180a9655f33d85fbf8c575aa6423558d5e8bc8
scottmertz <rezzkeepsitreal@gmail.com>
2026-07-28 17:56:34 -0400

    Initial project setup — Spring Boot scaffold

---
+++ grillogic/src/main/java/grillogic/GrillogicApplication.java
@@ -0,0 +1,13 @@
+package grillogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrillogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrillogicApplication.class, args);
+	}
+
+}
---
+++ grillogic/src/test/java/grillogic/GrillogicApplicationTests.java
@@ -0,0 +1,13 @@
+package grillogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrillogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
